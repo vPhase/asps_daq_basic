7865b0620e052f2f20fd37a1b8eefab1e92332cc
Patrick Allison <allison.122@osu.edu>
2016-11-06 16:08:45 -0500

    Initial add.

---
+++ asps_daq_basic.ino
@@ -0,0 +1,546 @@
+#include <WebServer.h>
+#include <TivaTwoWire.h>  
+#include <Cmd.h>
+#include <Ethernet.h>
+#include <EthernetBootloader.h>
+
+#include "driverlib/i2c.h"
+#include "driverlib/uart.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_nvic.h"
+#include "inc/hw_flash.h"
+#include "driverlib/sysctl.h"
+
+const char *cmd_banner = ">>> ASPS-DAQ Basic Setup Interface";
+const char *cmd_prompt = "ADAQ> ";
+const char *cmd_unrecog = "Unknown command.";
+
+#define BOOTCFG_MASK      0x7FFF00EC
+// NW = 0, PORT = 0 (A), pin = 2 (010), POL = 0 (Low), EN = 0,
+// KEY = 1, DBG1 = 1, DBG0 = 0.
+// so byte 1 is 000 010 0 0 = 0x08
+#define ASPS_DAQ_BOOTCFG  0x7FFF08FE
+
+#define VERSION "v0.1"
+
+HardwareSerial *bridgeSerial = NULL;
+unsigned char bridgeExitMatch = 0;
+#define EXIT_LENGTH 3
+#define EXIT_CHAR '+'
+
+typedef enum {
+  ETHERNET_NOT_STARTED = 0,
+  ETHERNET_STARTED_WAIT_DHCP = 1,
+  ETHERNET_READY = 2
+} EthernetState;
+
+EthernetState eState;
+EthernetBootloader boot;
+
+#define SENSOR_UPDATE_PERIOD 1000
+unsigned long sensorUpdateTime = 0;
+// Our sensors:
+// We have:
+// 5 currents     (PK3, PE3, PE4, PE5, PD6) = (68, 26, 2, 6, 87) = (AUX, SBC, SFE, ARAFE, FIBER)
+// 6 faults       (PK2, PQ2, PP1, PQ1, PP0) = (67, 55, 44, 52, 43)
+// 3 temperatures 
+// On pins (PQ0, PD3, PD1, PD2, PD0) = (47, 7, 15, 42, 14)
+const unsigned char currentPins[5] =  { 68, 26, 2, 6, 87 };
+const unsigned char faultPins[5] = {67,55,44,52,43};
+const unsigned char onPins[5] = {47,7,15,42,14};
+unsigned char onState[5];
+
+typedef enum {
+  SENSOR_STATE_ADC0 = 0,
+  SENSOR_STATE_ADC1 = 1,
+  SENSOR_STATE_ADC2 = 2,
+  SENSOR_STATE_ADC3 = 3,
+  SENSOR_STATE_ADC4 = 4,
+  SENSOR_STATE_TEMPSENSOR = 5,
+  SENSOR_STATE_FAULTS = 6,
+  SENSOR_STATE_I2C_0 = 7,
+  SENSOR_STATE_I2C_0_READ = 8,
+  SENSOR_STATE_I2C_1 = 9,
+  SENSOR_STATE_I2C_1_READ = 10,
+  SENSOR_STATE_I2C_1_COMPLETE = 11,
+  SENSOR_STATE_WAIT = 12
+} SensorState;
+SensorState sState = SENSOR_STATE_ADC0;
+
+typedef struct {
+  unsigned int current[5];
+  unsigned char fault;
+  int temps[3];
+} Sensors;
+Sensors curSensors;
+
+WebServer webServer("", 80);
+
+void setup() {
+  uint32_t user0, user1;
+  uint32_t tmp;
+  
+  // Figure out the bootloader crap.
+  Serial.begin(9600);  
+  Serial.println("ASPS-DAQ Basic " VERSION);
+  // Check the BOOTCFG register.
+  tmp = HWREG(FLASH_BOOTCFG);
+  if (tmp & FLASH_BOOTCFG_NW) {
+    Serial.println("Updating BOOTCFG...");
+    tmp = (tmp & BOOTCFG_MASK) | (ASPS_DAQ_BOOTCFG & ~BOOTCFG_MASK);
+    HWREG(FLASH_FMD) = tmp;
+    HWREG(FLASH_FMA) = 0x75100000;
+    HWREG(FLASH_FMC) = FLASH_FMC_WRKEY | FLASH_FMC_COMT;
+    while (HWREG(FLASH_FMC) & FLASH_FMC_COMT);
+    Serial.println("BOOTCFG updated.");
+  }  else {
+    Serial.print("BOOTCFG up-to-date: ");
+    Serial.println(tmp, HEX);
+  }
+  // print out the Ethernet MAC
+  getMacAddress(0, NULL);
+  ROM_FlashUserGet(&user0, &user1);
+  if (user0 == 0xFFFFFFFF && user1 == 0xFFFFFFFF) {
+    Serial.println("Not starting Ethernet - no MAC address");
+    eState = ETHERNET_NOT_STARTED;
+  } else {    
+    Ethernet.begin_nonblock();
+    Ethernet.enableLinkLed();
+    Ethernet.enableActivityLed();
+    eState = ETHERNET_STARTED_WAIT_DHCP;
+  }
+  // set up the FAULT pulls, on state
+  for (unsigned int i=0;i<5;i++) {
+    pinMode(faultPins[i], INPUT_PULLUP);
+    onState[i] = 1;
+    digitalWrite(onPins[i], 0);
+  }
+  Serial.end();
+  Serial1.begin(9600);
+  Serial4.begin(9600);
+  Serial5.begin(9600);
+  Serial7.begin(9600);
+  // this command crap needs to be fixed. I hate these libraries.
+  cmdInit(38400);
+  cmdAdd("getmac", getMacAddress);
+  cmdAdd("setmac", setMacAddress);
+  cmdAdd("savemac", saveMacAddress);
+  cmdAdd("bridge", serialBridge);
+  cmdAdd("ip", showip);
+  cmdAdd("status", printStatus);
+  cmdAdd("control", control);
+  analogRead(TEMPSENSOR);
+  Wire.begin();
+}
+
+int control(int argc, char **argv) {
+  unsigned char ch;
+  unsigned char st;
+  
+  argc--;
+  argv++;
+  if (argc < 2) {
+    Serial.println("control [0-4] [0-1]");
+    return 0;
+  }
+  ch = atoi(*argv);
+  argv++;
+  st = atoi(*argv);
+  if (ch > 4) {
+    Serial.println("control [0-4] [0-1]");
+    return 0;
+  }
+  if (st) {
+    pinMode(onPins[ch], INPUT);
+    onState[ch] = 1;
+  }  else {
+    pinMode(onPins[ch], OUTPUT);
+    onState[ch] = 0;
+  }
+  return 0;
+}
+
+int printStatus(int argc, char **argv) {
+  for (unsigned int i=0;i<5;i++) {
+    Serial.print("Current #");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.print(curSensors.current[i]);
+    Serial.println(" mA");
+  }
+  Serial.print("On   : ");
+  for (unsigned int i=0;i<5;i++) {
+    if (onState[i]) {
+      Serial.print(i);
+      Serial.print(" ");
+    } else {
+      Serial.print("  ");
+    }
+  }
+  Serial.println();
+  Serial.print("Fault: ");
+  for (unsigned int i=0;i<5;i++) {
+    if (curSensors.fault & (1<<i)) Serial.print(i);
+    else Serial.print(" ");
+    if (i != 4) Serial.print(" ");
+    else Serial.println();
+  }
+  for (unsigned int i=0;i<3;i++) {
+    Serial.print("Temp #");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.print(curSensors.temps[i]);
+    Serial.println(" C");
+  }
+  return 0;
+}
+
+int showip(int argc, char **argv) {
+  if (eState == ETHERNET_NOT_STARTED) {
+    Serial.println("No MAC address");
+    return 0;
+  }
+  if (eState == ETHERNET_STARTED_WAIT_DHCP) {
+    Serial.println("No IP address");
+    return 0;
+  }
+  Serial.println(Ethernet.localIP());
+  return 0;
+}
+
+int getMacAddress(int argc, char **argv) {
+  unsigned char tmp;
+  uint32_t user0, user1;
+  // print out the Ethernet MAC
+  ROM_FlashUserGet(&user0, &user1);  
+  Serial.print("MAC ");
+  tmp = (user0 >> 0) & 0xFF;
+  if (tmp < 15) Serial.print("0");
+  Serial.print(tmp, HEX);
+  Serial.print(":");
+  tmp = (user0 >> 8) & 0xFF;
+  if (tmp < 15) Serial.print("0");
+  Serial.print(tmp, HEX);
+  Serial.print(":");
+  tmp = (user0 >> 16) & 0xFF;
+  if (tmp < 15) Serial.print("0");
+  Serial.print(tmp, HEX);
+  Serial.print(":");
+  tmp = (user1 >> 0) & 0xFF;
+  if (tmp < 15) Serial.print("0");
+  Serial.print(tmp, HEX);
+  Serial.print(":");
+  tmp = (user1 >> 8) & 0xFF;
+  if (tmp < 15) Serial.print("0");
+  Serial.print(tmp, HEX);
+  Serial.print(":");
+  tmp = (user1 >> 16) & 0xFF;
+  if (tmp < 15) Serial.print("0");
+  Serial.println(tmp, HEX);
+  return 0;
+}
+
+int setMacAddress(int argc, char **argv) {
+  uint32_t user0;
+  uint32_t user1;
+  uint8_t tmp[6];
+  argc--;
+  argv++;
+  if (argc < 6) {
+    Serial.println("setmac needs a MAC address: 6 hex digits");
+    return 0;
+  }
+  ROM_FlashUserGet(&user0, &user1);  
+  if (user0 != 0xFFFFFFFF ||
+      user1 != 0xFFFFFFFF) {
+    Serial.println("MAC address is already programmed, can't overwrite.");
+    return 0;
+  }
+  Serial.print("Updating MAC to ");
+  for (unsigned int i=0;i<6;i++) {
+    tmp[i] = strtoul(argv[i], NULL, 16);
+    if (i) Serial.print(":");
+    Serial.print(tmp[i], HEX);
+  }
+  Serial.println();
+  user0 = tmp[0];
+  user0 = user0 | (tmp[1] << 8);
+  user0 = user0 | (tmp[2] << 16);
+  user1 = tmp[3];
+  user1 = user1 | (tmp[4] << 8);
+  user1 = user1 | (tmp[5] << 16);
+  ROM_FlashUserSet(user0, user1);
+  return 0;
+}
+
+int saveMacAddress(int argc, char **argv) {
+  ROM_FlashUserSave();
+  Serial.println("--- MAC ADDRESS HAS BEEN COMMITTED ---");
+  return 0;
+}
+
+#define PORT_BRIDGE_MAX 4
+int serialBridge(int argc, char **argv) {
+  HardwareSerial *thisSerial;
+  unsigned int port;
+  argc--;
+  argv++;
+  if (!argc) {
+    Serial.println("bridge needs a port to bridge to");
+    return 0;
+  }
+  port = strtoul(*argv, NULL, 0);
+  if (port < PORT_BRIDGE_MAX) {
+    unsigned char exit_match = 0;
+    switch (port) {
+      case 0: bridgeSerial = &Serial1; break;
+      case 1: bridgeSerial = &Serial4; break;
+      case 2: bridgeSerial = &Serial5; break;
+      case 3: bridgeSerial = &Serial7; break;
+    }
+  } else {
+    Serial.println("invalid port");
+    return 0;
+  }
+  return 1;
+}
+
+void sensorLoop()
+{
+  int32_t temp;
+  unsigned char faultBit;
+  switch (sState) {
+    case SENSOR_STATE_ADC0:
+    case SENSOR_STATE_ADC1:
+    case SENSOR_STATE_ADC2:
+    case SENSOR_STATE_ADC3:
+    case SENSOR_STATE_ADC4:
+      temp = analogRead(currentPins[(unsigned char) sState]);
+      // IMON output is 53 uA/A. Then passed through 10k resistor, gives 0.53 V/A or 0.53 mV/mA
+      // Max readout is 4096, so 3300 mV/4096 ADC = 0.8056 mV/ADC
+      // So conversion is 0.8056 mV/ADC / 0.53 mV/mA = 1.52 mA / ADC.
+      // Sticking to integers, this is times 389, downshift by 8.
+      temp = (temp*389);
+      temp = temp >> 8;
+      curSensors.current[(unsigned char) sState] = temp;
+      sState = (SensorState) ((unsigned char) sState + 1);
+      break;
+    case SENSOR_STATE_TEMPSENSOR:
+      temp = analogRead(TEMPSENSOR);
+      // Now we need (1475 - ((2475/4096)*adc)/10
+      // or (295 - 0.1208*adc)/2
+      // or (295 - (31*adc)>>8)>>1
+      temp = (temp*31);
+      temp = temp >> 8;
+      temp = 295 - temp;
+      temp = temp >> 1;
+      curSensors.temps[0] = temp;
+      sState = (SensorState) ((unsigned char) sState + 1);
+      break;
+    case SENSOR_STATE_FAULTS:
+      curSensors.fault = 0;
+      faultBit = 1;
+      for (unsigned int i=0;i<5;i++) {
+        if (!digitalRead(faultPins[i])) curSensors.fault |= faultBit;
+        faultBit <<= 1;
+      }
+      sState = (SensorState) ((unsigned char) sState + 1);
+      break;
+   case SENSOR_STATE_I2C_0:
+      Wire.beginTransmission(0x48);
+      Wire.write(0x00);
+      Wire.endTransmission_nonblock();
+      sState = (SensorState) ((unsigned char) sState + 1);
+      break;
+   case SENSOR_STATE_I2C_0_READ:
+      if (Wire.status() == I2C_MASTER_ERR_BUSY) return;
+      if (Wire.status() != I2C_MASTER_ERR_NONE) {
+        Serial.println("I2C> I2C error");
+        sState = SENSOR_STATE_WAIT;
+        sensorUpdateTime = millis() + SENSOR_UPDATE_PERIOD;
+        break;
+      }
+      Wire.requestFrom_nonblock(0x48, 1);
+      sState = (SensorState) ((unsigned char) sState + 1);
+      break;
+   case SENSOR_STATE_I2C_1:
+      if (Wire.status() == I2C_MASTER_ERR_BUSY) return;
+      if (Wire.status() != I2C_MASTER_ERR_NONE) {
+        Serial.println("I2C> I2C error");
+        sState = SENSOR_STATE_WAIT;
+        sensorUpdateTime = millis() + SENSOR_UPDATE_PERIOD;
+        break;
+      }
+      curSensors.temps[1] = Wire.read();
+      Wire.beginTransmission(0x48);
+      Wire.write(0x01);
+      Wire.endTransmission_nonblock();
+      sState = (SensorState) ((unsigned char) sState + 1);
+      break;
+   case SENSOR_STATE_I2C_1_READ:
+      if (Wire.status() == I2C_MASTER_ERR_BUSY) return;
+      if (Wire.status() != I2C_MASTER_ERR_NONE) {
+         Serial.println("I2C> I2C error");
+         sState = SENSOR_STATE_WAIT;
+         sensorUpdateTime = millis() + SENSOR_UPDATE_PERIOD;
+         break;
+      }
+      Wire.requestFrom_nonblock(0x48, 1);
+      sState = (SensorState) ((unsigned char) sState + 1);
+      break;
+  case SENSOR_STATE_I2C_1_COMPLETE:
+      if (Wire.status() == I2C_MASTER_ERR_BUSY) return;
+      if (Wire.status() == I2C_MASTER_ERR_NONE) {
+        curSensors.temps[2] = Wire.read();        
+      } else {
+        Serial.println("I2C> I2C error");
+      }
+      sensorUpdateTime = millis() + SENSOR_UPDATE_PERIOD;
+      sState = SENSOR_STATE_WAIT;
+      break;
+  case SENSOR_STATE_WAIT:
+      if ((long) (millis() - sensorUpdateTime) > 0) {
+        sState = SENSOR_STATE_ADC0;
+        break;
+      }
+  } 
+}
+
+void loop() {
+  char c;
+
+  // Sensor handling.
+  sensorLoop();
+  
+  // Serial handling.
+  if (bridgeSerial == NULL)
+    cmdPoll();
+  else {
+    char c;
+    while (Serial.available() && bridgeExitMatch < EXIT_LENGTH) {
+      c = Serial.read();
+      if (c == EXIT_CHAR) bridgeExitMatch++;
+      else bridgeExitMatch = 0;
+      bridgeSerial->write(c);
+    }
+    if (bridgeExitMatch >= EXIT_LENGTH) {
+      bridgeSerial = NULL;
+      bridgeExitMatch = 0;
+      cmdPrompt();
+    }
+  }
+  // Serial1 handling.
+  while (Serial1.available()) {
+    c = Serial1.read();
+    if (bridgeSerial == &Serial1) Serial.write(c);
+  }
+  // Serial4 handling.
+  while (Serial4.available()) {
+    c = Serial4.read();
+    if (bridgeSerial == &Serial4) Serial.write(c);
+  }
+  // Serial5 handling.
+  while (Serial5.available()) {
+    c = Serial5.read();
+    if (bridgeSerial == &Serial5) Serial.write(c);
+  }
+  // Serial7 handling.
+  while (Serial7.available()) {
+    c = Serial7.read();
+    if (bridgeSerial == &Serial7) Serial.write(c);
+  }
+
+  if (eState == ETHERNET_STARTED_WAIT_DHCP) {
+    if (Ethernet.ready()) {
+      eState = ETHERNET_READY;
+      if (bridgeSerial == NULL) {
+        Serial.print("DHCP Complete: ");
+        Serial.println(Ethernet.localIP());
+      }
+      boot.begin();
+      webServer.begin();
+      webServer.addCommand("index.html", &defaultPage);
+      webServer.setDefaultCommand(&defaultPage);
+    }
+  }
+  if (eState == ETHERNET_READY) {
+    boot.handle();
+    webServer.processConnection();
+  }
+}
+
+const char *output_labels[5] = {
+  "AUX  ",
+  "SBC  ",
+  "AUX2 ",
+  "ARAFE",
+  "FIBER"   
+};
+
+void defaultPage(WebServer &server, WebServer::ConnectionType type, char *url_tail, bool tail_complete)
+{
+  char name[5];
+  char value[5];
+  URLPARAM_RESULT rc;
+  unsigned int param_number;
+  
+  const char *startPage = "<html>"
+                            "<head>"
+                            "<title>ASPS-DAQ Main Page</title>"
+                            "<body>"
+                            "<h1>ASPS-DAQ Main Page</h1>"
+                            "<br>"
+                            "<h2>Sensors</h2>"
+                            "<form action=\"index.html\" method=\"get\">"
+                            "<p>";
+  const char *tempStart   = "<input type=\"submit\" value=\"Change Outputs\"></form></p><p>"
+                            "Temperature:"; 
+  const char *endPage     = "</p></body></html>";
+  
+  unsigned int i;
+  server.httpSuccess();
+  if (strlen(url_tail)) {
+    while (strlen(url_tail)) {
+      rc = server.nextURLparam(&url_tail, name, 5, value, 5);
+      if (rc != URLPARAM_EOS) {
+        param_number = atoi(name);
+        if (param_number < 5) {
+          if (atoi(value)) {
+             pinMode(onPins[param_number], INPUT);
+             onState[param_number] = 1;
+          } else {
+             pinMode(onPins[param_number], OUTPUT);
+             onState[param_number] = 0;
+          }
+        }
+      }
+    }
+  }
+  server.print(startPage);
+  for (i=0;i<5;i++) {
+    server.print("<h3>");
+    server.print(output_labels[i]);
+    server.print("</h3>");
+    server.print("<input type=\"radio\" name=\"");
+    server.print(i);
+    server.print("\" value=\"1\"");
+    if (onState[i]) server.print(" checked");
+    server.print(">On<input type=\"radio\" name=\"");
+    server.print(i);
+    server.print("\" value=\"0\"");
+    if (!onState[i]) server.print(" checked");
+    server.print(">Off<br>");
+    server.print("Current: ");
+    server.print(curSensors.current[i]);
+    server.print(" mA<br>");    
+  }
+  server.print(tempStart);
+  for (i=0;i<3;i++) {
+    server.print(" ");
+    server.print(curSensors.temps[i]);
+    server.print(" C");
+  }
+  server.print(endPage);
+}
+
